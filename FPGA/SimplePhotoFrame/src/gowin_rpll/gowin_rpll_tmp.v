9d33df73471d556f19b03758b823eea78c6d36a1
HRA! <hra@mvj.biglobe.ne.jp>
2026-05-31 11:59:55 +0900

    PLL追加

---
+++ FPGA/SimplePhotoFrame/src/gowin_rpll/gowin_rpll_tmp.v
@@ -0,0 +1,19 @@
+//Copyright (C)2014-2025 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//Tool Version: V1.9.11.02 (64-bit)
+//Part Number: GW2AR-LV18QN88C8/I7
+//Device: GW2AR-18
+//Device Version: C
+//Created Time: Sat May 30 21:18:52 2026
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_rPLL your_instance_name(
+        .clkout(clkout), //output clkout
+        .clkoutd(clkoutd), //output clkoutd
+        .clkin(clkin) //input clkin
+    );
+
+//--------Copy end-------------------
